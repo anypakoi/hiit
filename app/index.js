4482f5b6cefebbe70bc20e854369246079806ce1
anypakoi <lucademi16@gmail.com>
2024-12-18 13:09:43 -0300

    bug

--- app/index.js
+++ app/index.js
@@ -1,17 +1,39 @@
 import AppNavigator from '../navigation/AppNavigator';
-import React from 'react';
-import { Provider } from 'react-redux';
+import React, { useEffect } from 'react';
+import { Provider, useDispatch } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../redux/store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { setInitialConfig } from '../redux/slices/configSlice';
 
-export default class App extends React.Component {
-  render() {
-    return (
+// Componente separado para la inicialización
+const InitializeApp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadSavedConfig = async () => {
+      try {
+        const savedConfig = await AsyncStorage.getItem('appConfig');
+        if (savedConfig) {
+          dispatch(setInitialConfig(JSON.parse(savedConfig)));
+        }
+      } catch (error) {
+        console.error('Error loading saved config:', error);
+      }
+    };
+
+    loadSavedConfig();
+  }, []);
+
+  return <AppNavigator />;
+};
+
+export default function App() {
+  return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <AppNavigator />
+        <InitializeApp />
       </PersistGate>
     </Provider>
   );
-  }
 }
--- redux/slices/configSlice.js
+++ redux/slices/configSlice.js
@@ -1,23 +1,26 @@
 // configSlice.js
 import { createSlice } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const configSlice = createSlice({
+const initialState = {
+  soundAlarm: "alarm clock",
+};
+
+export const configSlice = createSlice({
   name: 'config',
-  initialState: {
-    // Configuraciones por defecto
-    soundAlarm: 'alarm clock',
-
-  },
+  initialState,
   reducers: {
-    // Método genérico para actualizar cualquier configuración
     updateConfig: (state, action) => {
       const { key, value } = action.payload;
       state[key] = value;
+      // Guardar en AsyncStorage cuando se actualiza
+      AsyncStorage.setItem('appConfig', JSON.stringify(state));
     },
-  }
+    setInitialConfig: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+  },
 });
 
-export const { 
-  updateConfig, 
-} = configSlice.actions;
+export const { updateConfig, setInitialConfig } = configSlice.actions;
 export default configSlice.reducer;
