1c316e3c8a8220beafb4efd01d039e6fd604c985
luca <lucademi16@gmail.com>
2025-01-30 15:46:35 -0300

    testing error

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,1 @@
-module.exports = {
-  presets: [
-    ['babel-preset-expo'],
-    ['@babel/preset-env', { loose: true }]
-  ],
-  plugins: [
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    '@babel/plugin-transform-modules-commonjs'
-  ]
-};
+module.exports = { presets: ['babel-preset-expo']};
